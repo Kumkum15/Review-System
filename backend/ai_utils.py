670deb869e45375bf73e828a8e2567deba558bc6
Kumkum15 <kumkumhirani15@gmail.com>
2025-12-07 13:41:36 +0530

    Fixing API KEY problem

--- backend/ai_utils.py
+++ backend/ai_utils.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY = os.getenv("GENAPI_KEY")
+API_KEY = os.getenv("GEMINI_API_KEY")
 MODEL = os.getenv("GEN_MODEL", "gemini-1.5-flash-latest")
 
 BASE_URL_TEMPLATE = (
